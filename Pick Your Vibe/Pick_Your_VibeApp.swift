a7f5fe4259c9ec96f2b30a73df65eb9261d374f7
Asaf Baibekov <asafall21@gmail.com>
2025-07-09 14:28:29 +0300

    Initial Commit

---
+++ Pick Your Vibe/Pick_Your_VibeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pick_Your_VibeApp.swift
+//  Pick Your Vibe
+//
+//  Created by Asaf Baibekov on 09/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct Pick_Your_VibeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
