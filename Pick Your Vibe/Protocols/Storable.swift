ecce691912c1b5d652e733a4ae2dd306267e48cb
Asaf Baibekov <asafall21@gmail.com>
2025-07-09 22:00:55 +0300

    create a storable protocol and any storable generic class

---
+++ Pick Your Vibe/Protocols/Storable.swift
@@ -0,0 +1,23 @@
+//
+//  Storable.swift
+//  Pick Your Vibe
+//
+//  Created by Asaf Baibekov on 09/07/2025.
+//
+
+
+protocol Storable {
+
+    associatedtype StoredType
+
+    func save(_ value: StoredType) async throws
+
+    func load() async throws -> StoredType?
+}
+
+extension Storable {
+
+    func eraseToAnyStorable() -> AnyStorable<StoredType> {
+        return AnyStorable(self)
+    }
+}
---
+++ Pick Your Vibe/Storables/AnyStorable.swift
@@ -0,0 +1,27 @@
+//
+//  AnyStorable.swift
+//  Pick Your Vibe
+//
+//  Created by Asaf Baibekov on 09/07/2025.
+//
+
+
+final class AnyStorable<T>: Storable {
+    typealias StoredType = T
+
+    private let _save: (T) async throws -> Void
+    private let _load: () async throws -> T?
+
+    init<S: Storable>(_ store: S) where S.StoredType == T {
+        self._save = store.save
+        self._load = store.load
+    }
+
+    func save(_ value: T) async throws {
+        try await _save(value)
+    }
+
+    func load() async throws -> T? {
+        try await _load()
+    }
+}
